bcfe0a408e8f310001f5e93b17bfed8de048cf29
Ramzan <ramkurbanov10@yandex.ru>
2022-08-25 20:44:55 +0300

    add initial

---
+++ pet-clinic/src/test/java/com/ramz/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramz.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
