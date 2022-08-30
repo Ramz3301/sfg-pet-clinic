1d6c142676fc7e6731079741cee34cc7665569eb
Ramzan <ramkurbanov10@yandex.ru>
2022-08-30 08:52:54 +0300

    adding crud service

--- pet-clinic/pet-clinic-data/src/main/java/service/OwnerService.java
+++ pet-clinic/pet-clinic-data/src/main/java/service/OwnerService.java
@@ -1,16 +1,13 @@
 package service;
 
 import com.ramz.petclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic/pet-clinic-data/src/main/java/service/VetService.java
+++ pet-clinic/pet-clinic-data/src/main/java/service/VetService.java
@@ -4,11 +4,5 @@ import com.ramz.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
