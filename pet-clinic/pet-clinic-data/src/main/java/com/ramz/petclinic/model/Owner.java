4d6f2313d6ee2e8b9842131e909d31ed2cf3ec5f
Ramzan <ramkurbanov10@yandex.ru>
2022-09-07 19:52:35 +0300

    adding visit entity

--- pet-clinic/pet-clinic-data/src/main/java/com/ramz/petclinic/model/Owner.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/ramz/petclinic/model/Owner.java
@@ -2,6 +2,19 @@ package com.ramz.petclinic.model;
 
 import lombok.Builder;
 
+import java.util.Set;
+
 //@Builder
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
 }
